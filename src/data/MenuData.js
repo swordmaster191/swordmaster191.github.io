bffec848a94accd2c1253d64464151254879c06c
SwordMaster <43595245+SwordMaster191@users.noreply.github.com>
2022-02-23 02:54:26 +0700

    Updates

--- src/data/MenuData.js
+++
@@ -1,6 +0,0 @@
-//Links to menu file
-export const menuData = [
-    {title: "About", link: "#about"},
-    {title: "Experiences", link: "#experiences"},
-    {title: "Contact", link: "#contact"},
-]
