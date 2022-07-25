90cdeb14bec94d6ac79471e3edd5df05e4700230
SwordMaster <43595245+SwordMaster191@users.noreply.github.com>
2022-07-26 02:27:02 +0700

    Add query to all projects page

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,7 +7,7 @@ const Project = ({description, title, github, stack, url, image, index}) => {
     <article className="project">
       <GatsbyImage image={image.localFile.childImageSharp.gatsbyImageData} className="project-img" />
       <div className="project-info">
-        <span className="project-number">0{index + 1}</span>
+        <span className="project-number">0{index + 1}.</span>
         <h3>{title}</h3>
         <p className="project-desc">{description}</p>
         <div className="project-stack">
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,10 +2,37 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Projects from "../components/Projects"
+import {Image, GatsbyImage} from "gatsby-plugin-image"
 // ...GatsbyImageSharpFluid
 
 const ProjectsPage = () => {
   return <h2>projects page</h2>
 }
+
+export const query = graphql`
+  {
+    allStrapiProject {
+      nodes {
+        github
+        id
+        description
+        title
+        url
+        stack {
+          id
+          title
+        }
+        image {
+          localFile {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
 
 export default ProjectsPage
